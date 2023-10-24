3d45e75c04e193b86e24d98d3ace6fc65a98747c
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-25 00:24:41 +0100

    Added password validation feature to ValidationTools

--- tests/test_validation_tools.py
+++ tests/test_validation_tools.py
@@ -6,3 +6,11 @@ from lib.validation_tools import ValidationTools
 def test_init_validation_tools():
     instance = ValidationTools('value')
     assert instance.value == 'value'
+
+def test_password_validator_truthy_result():
+    instance = ValidationTools('value')
+    assert instance.password_validator('Test1234!','!@£', 8) == True
+
+def test_password_validator_false_result():
+    instance = ValidationTools('value')
+    assert instance.password_validator('Test1234','!@£', 8) == False
