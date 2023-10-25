20b6fa4a10c84643ca77c3a6a28c0c09f0d5362f
musty0123 <REDACTED>
2023-10-25 11:25:12 +0100

    Template for accomodation form routes

--- app.py
+++ app.py
@@ -20,3 +20,25 @@ def get_index():
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+
+# HTTP Method that gets user id and redirects to accomadation form Page
+# It will be a get request --> we need user id
+# we need to validate it 
+# and then redirect 
+
+@app.route('/post_accomodation/<user>')
+def success(user):
+    # check to see if user is in database 
+    # if sucessful go to accomadation page
+    # if unsuccessful go to login page
+
+    # question : how do we get user input, will the data be persisted throughout the web app ? 
+    # question : is it good practice to have user id passed through the url? 
+    pass
+
+@app.route('/post_accomodation/<user>', methods = ['POST'])
+def new_accomodation():
+    # this method will add the necessary information to the accomodations table, check table for the neccessary colunns, this is onclick after the form has been submitted
+    pass 
+
+    
