37132c303a6e5f943c4b9f41d00f4ea0fec45454
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-25 14:45:48 +0100

    Added model booking class and corresponding tests

---
+++ lib/booking.py
@@ -0,0 +1,5 @@
+class Booking:
+    def __init__(self, user_id: int, accommodation_id: int, is_booked: bool):
+        self.user_id = user_id
+        self.accommodation_id = accommodation_id
+        self.is_booked = is_booked
