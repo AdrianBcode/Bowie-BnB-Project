78267034081504e5f21ef012e4bb7f2286d63902
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-25 14:52:15 +0100

    Added booking repository class and corresponding tests

---
+++ lib/booking_repository.py
@@ -0,0 +1,15 @@
+class BookingRepository:
+    def __init__(self):
+        pass
+
+    def create_booking(self):
+        pass
+    
+    def find_booking(self):
+        pass
+
+    def update_booking(self):
+        pass
+
+    def delete_booking(self):
+        pass
