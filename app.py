0bdff2a8bc7bcab7b114a40e93e6256d9b63c6e0
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-25 01:19:32 +0100

    Added homepage route to app.py and appropriate tests

--- app.py
+++ app.py
@@ -5,16 +5,14 @@ from lib.database_connection import get_flask_database_connection
 # Create a new Flask app
 app = Flask(__name__)
 
-# == Your Routes Here ==
-
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5000/index
-
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+# Creates the default route (homepage in this case)
+@app.route('/', methods=['GET'])
+def get_homepage():
+    return render_template('homepage.html')
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
