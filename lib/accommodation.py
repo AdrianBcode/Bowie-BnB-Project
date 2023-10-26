93c6428bc5fc23147a7309a35837d7963adc68b5
Iain <REDACTED>
2023-10-26 15:09:04 +0100

    Fixed 4 tests

--- lib/accommodation.py
+++ lib/accommodation.py
@@ -3,13 +3,15 @@ class Accommodation:
     def __init__(self, id, place_name, host_id, img_path, description, price):
         self.id = id
         self.place_name = place_name
-        self.description = description
-        self.price = price
         self.host_id = host_id
         self.img_path = img_path
+        self.description = description
+        self.price = price
+        
+        
     
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"Album({self.id}, {self.place_name}, {self.description}, {self.price}, {self.host_id}, {self.img_path})"
+        return f"Accommodation({self.id}, {self.host_id}, {self.place_name}, {self.img_path}, {self.description}, {self.price})"
