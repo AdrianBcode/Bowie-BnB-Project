5962728983cdb801cd7fab8324cbfc39e10d6210
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-26 01:43:40 +0100

    Added create user test

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -12,3 +12,9 @@ def test_all(db_connection):
     assert repository.all() == [
         User(1, "Angie", "Angie@example.com", "changes")
     ]
+
+def test_create_user(db_connection):
+    db_connection.seed("seeds/bowie_bnb_seed.sql")
+    repository = UserRepository(db_connection)
+    repository.create('George','george@none.none','12345678!')
+    assert User(2,'George','george@none.none','12345678!') in repository.all()
