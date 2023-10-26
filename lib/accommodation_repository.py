e500c23c8c90122156d9a7765158b9f3401ad2e9
NDR-114 <81937614+NDR-114@users.noreply.github.com>
2023-10-26 01:54:58 +0100

    Added find method and updated other methods to include new table columns

--- lib/accommodation_repository.py
+++ lib/accommodation_repository.py
@@ -8,11 +8,17 @@ class AccommodationRepository:
     def all(self):
         rows = self.connection.execute("SELECT * FROM accommodations;")
         return [
-            Accommodation(row['id'], row['place_name'], row['start_date'], row['end_date'], row['host_id']) for row in rows
+            Accommodation(row['id'],row['place_name'],row['description'],row['price'],row['host_id'],row['img_path']) for row in rows
         ]
     
     def create(self, accommodation):
         self.connection.execute(
-            "INSERT INTO accommodations (place_name, start_date, end_date, host_id) VALUES (%s, %s, %s, %s)",
-            [accommodation.place_name, accommodation.start_date, accommodation.end_date, accommodation.host_id]
+            "INSERT INTO accommodations (place_name, host_id, img_path, description, price) VALUES (%s, %s, %s, %s)",
+            [accommodation.place_name, accommodation.host_id, accommodation.img_path, accommodation.description,accommodation.price]
         )
+    
+    def find(self, place_name):
+        rows = self.connection.execute(
+            "SELECT * from accommodations WHERE place_name = %s",[place_name])
+        row = rows[0]
+        return Accommodation(row['id'],row['place_name'],row['description'],row['price'],row['host_id'],row['img_path'])
